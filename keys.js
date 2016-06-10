f7c4c421f9f3b5cfe7b517c268f29c52f36177f0
John Dispennette <jtdartworks@gmail.com>
2016-06-10 09:10:03 -0500

    created Twitter keys and access tokens have not been able to figure out why my .gitignore is not working/being created

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '<Gwa0KHrbnOFDc8FBLTnnKUHm1>',
+  consumer_secret: '<LgnmOCkNegFPbvMLnCcXq9l8TPLXOq1gk2OBnsqCzuVzeV4f5W>',
+  access_token_key: '<	3248122179-zVQNQCJ2RRlKwH4wt8Y7tTvafTJ2crby8T8X8cQ>',
+  access_token_secret: '<R3Ahs7lxPstzhvdFOxkmSvCyvhcxzQtpfyIZlYXtg9Y0P>',
 }
