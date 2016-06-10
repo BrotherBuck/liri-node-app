87172d55c79247d40e36dae52940eb3c79cc5217
John Dispennette <jtdartworks@gmail.com>
2016-06-10 09:31:31 -0500

    pseudo code liri.js added ;log.text

---
+++ liri.js
@@ -0,0 +1,17 @@
+/*
+Make a JavaScript file named liri.js
+
+At the top of the liri.js file make it so you grab the data from keys.js and store it into a variable to use
+
+Make it so liri.js can take in one of the following arguments
+
+my-tweets
+
+spotify-this-song
+
+movie-this
+
+do-what-it-says
+
+*/
+
