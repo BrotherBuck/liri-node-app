d8aaceceb2950d202e9bb287aa89c169f4142ba9
John Dispennette <jtdartworks@gmail.com>
2016-06-11 07:33:08 -0500

    final commit

--- liri.js
+++ liri.js
@@ -5,8 +5,31 @@ At the top of the liri.js file make it so you grab the data from keys.js and sto
 
 Make it so liri.js can take in one of the following arguments
 
-my-tweets
+my-tweets*/
 
+var fs = require('fs');
+
+if (process.argv[2] === 'my-tweets') {
+
+var Twitter = require('twitter');
+var keys = require('./keys.js');
+
+
+var client = new Twitter({
+  consumer_key: keys.twitterKeys.consumer_key,
+  consumer_secret: keys.twitterKeys.consumer_secret,
+  access_token_key: keys.twitterKeys.access_token_key,
+  access_token_secret: keys.twitterKeys.access_token_secret,
+});
+ 
+var params = {screen_name: '@jtdartworks'};
+client.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+/*
 spotify-this-song
 
 movie-this
